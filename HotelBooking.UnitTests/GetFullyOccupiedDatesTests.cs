cf627060c738012ead0b917e36b9469803d18e19
Nelson Veríssimo <nelson.sverissimo@gmail.com>
2026-09-09 18:54:09 +0200

    Added more tests

--- HotelBooking.UnitTests/GetFullyOccupiedDatesTests.cs
+++ HotelBooking.UnitTests/GetFullyOccupiedDatesTests.cs
@@ -17,17 +17,15 @@ public class GetFullyOccupiedDatesTests
         //Arrange 
         var rooms = BookingTestData.DefaultRooms;
         var bookings = new List<Booking>();
-        
         var bookingManager = MoqBookingManager.CreateBookingManager(rooms, bookings);
-
-        var start = DateTime.Today;
-        var end = start.AddDays(3);
         
         //Act 
-        var result = await bookingManager.GetFullyOccupiedDates(start, end);
+        var result = await bookingManager
+            .GetFullyOccupiedDates(BookingTestData.StartDateData, BookingTestData.EndDateData);
         
         //Assert 
         Assert.Empty(result);
+        Assert.NotNull(result);
     }
     
     [Theory]
@@ -41,9 +39,56 @@ public class GetFullyOccupiedDatesTests
         var bookingManager = MoqBookingManager.CreateBookingManager(rooms, bookings);
         
         //Act 
-        var result = await bookingManager.GetFullyOccupiedDates(BookingTestData.StartDate, BookingTestData.EndDate);
+        var result = await bookingManager
+            .GetFullyOccupiedDates(BookingTestData.StartDateData, BookingTestData.EndDateData);
         
         //Assert 
         Assert.Empty(result);
+    }
+    
+    [Fact]
+    public async Task GetFullyOccupiedDates_AllRoomsOccupied_ReturnsDate()
+    {
+        //Arrange 
+        var rooms = BookingTestData.DefaultRooms;
+        var bookings = new List<Booking>
+        {
+            BookingTestData.CreateBooking(1, rooms[0].Id),
+            BookingTestData.CreateBooking(2, rooms[1].Id),
+            BookingTestData.CreateBooking(3, rooms[2].Id)
+        };
+        
+        var bookingManager = MoqBookingManager.CreateBookingManager(rooms, bookings);
+        
+        //Act 
+        var result = await bookingManager
+            .GetFullyOccupiedDates(BookingTestData.StartDateData, BookingTestData.EndDateData);
+        
+        //Assert 
+        Assert.NotEmpty(result);
+        Assert.NotNull(result);
+        Assert.Contains(BookingTestData.StartDateData, result);
+        Assert.Contains(BookingTestData.EndDateData, result);
+    }
+    
+    [Theory]
+    [MemberData(
+        nameof(BookingTestData.AllRoomsOccupiedForSeveralDaysData),
+        MemberType = typeof(BookingTestData))]
+    public async Task GetFullyOccupiedDates_AllRoomsOccupiedForSeveralDays_ReturnsAllDates(List<Booking> bookings)
+    {
+        //Arrange 
+        var rooms = BookingTestData.DefaultRooms;
+        var bookingManager = MoqBookingManager.CreateBookingManager(rooms, bookings);
+        
+        //Act 
+        var result = await bookingManager
+            .GetFullyOccupiedDates(BookingTestData.StartDateData, BookingTestData.StartDateData.AddDays(30));
+        
+        //Assert 
+        //TODO: Add assertions to check that the result contains all the expected fully occupied dates based on the provided bookings.
+        Assert.NotEmpty(result);
+        Assert.Contains(BookingTestData.StartDateData.AddDays(2), result);
+        Assert.Contains(BookingTestData.StartDateData.AddDays(8), result);
     }
 }
--- HotelBooking.UnitTests/TestData/BookingTestData.cs
+++ HotelBooking.UnitTests/TestData/BookingTestData.cs
@@ -7,42 +7,66 @@ namespace HotelBooking.UnitTests.TestData;
 
 public static class BookingTestData
 {
-    public static readonly DateTime StartDate = DateTime.Today;
-    public static readonly DateTime EndDate = StartDate.AddDays(4);
+    public static readonly DateTime StartDateData = DateTime.Today;
+    public static readonly DateTime EndDateData = StartDateData.AddDays(4);
 
     public static List<Room> DefaultRooms =>
-        new()
-        {
-            new Room { Id = 1, Description = "Room 1" },
-            new Room { Id = 2, Description = "Room 2" },
-            new Room { Id = 3, Description = "Room 3" }
-        };
+    [
+        new Room { Id = 1, Description = "Room 1" },
+        new Room { Id = 2, Description = "Room 2" },
+        new Room { Id = 3, Description = "Room 3" }
+    ];
 
     public static TheoryData<List<Booking>> SomeRoomsOccupiedData =>
-        new()
-        {
-            // One room occupied (first theory case)
-            new List<Booking>
-            {
-                CreateBooking(1, 1)
-            },
+    [
 
-            // Two rooms occupied (second theory case)
-            new List<Booking>
-            {
-                CreateBooking(1, 1),
-                CreateBooking(2, 2)
-            }
-        };
+        // One room occupied (first theory case)
+        new List<Booking>
+        {
+            CreateBooking(1, 1)
+        },
+
+        // Two rooms occupied (second theory case)
+
+        new List<Booking>
+        {
+            CreateBooking(1, 1),
+            CreateBooking(2, 2)
+        }
+    ];
     
-    private static Booking CreateBooking(int id, int roomId)
+    public static TheoryData<List<Booking>> AllRoomsOccupiedForSeveralDaysData =>
+    [
+
+        // All rooms occupied with different overlapping dates
+        // first theory case
+        new List<Booking>
+        {
+            CreateBooking(1, 1, StartDateData, StartDateData.AddDays(10)),
+            CreateBooking(2, 2, StartDateData.AddDays(1), StartDateData.AddDays(15)),
+            CreateBooking(3, 3, StartDateData.AddDays(2), StartDateData.AddDays(17))
+        },
+        // second theory case
+        new List<Booking>
+        {
+            CreateBooking(1, 1, StartDateData, StartDateData.AddDays(8)),
+            CreateBooking(2, 2, StartDateData.AddDays(1), StartDateData.AddDays(8)),
+            CreateBooking(3, 3, StartDateData.AddDays(2), StartDateData.AddDays(10)),
+            
+            CreateBooking(1, 1, StartDateData.AddDays(9), StartDateData.AddDays(14)),
+            CreateBooking(2, 2, StartDateData.AddDays(9), StartDateData.AddDays(13)),
+            CreateBooking(3, 3, StartDateData.AddDays(11), StartDateData.AddDays(17))
+        }
+    ];
+    
+    public static Booking CreateBooking(int id, int roomId, DateTime? startDate = null, DateTime? endDate = null, bool? isActive = true)
     {
         return new Booking
         {
             Id = id,
-            StartDate = StartDate,
-            EndDate = EndDate,
-            IsActive = true,
+            StartDate = startDate ?? StartDateData,
+            EndDate = endDate ?? EndDateData,
+            IsActive = isActive ?? true,
             RoomId = roomId
         };
     }
