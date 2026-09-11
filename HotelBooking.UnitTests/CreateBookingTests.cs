6ebfa6b5ad12092621c2173cab6947a65e9a6f62
jean-jensen <JeanJensen911@gmail.com>
2026-09-11 21:25:26 +0700

    cleanup & cases 16-19

--- HotelBooking.UnitTests/CreateBookingTests.cs
+++ HotelBooking.UnitTests/CreateBookingTests.cs
@@ -11,7 +11,7 @@ namespace HotelBooking.UnitTests;
 public class CreateBookingTests
 {
 
-    private readonly ITestOutputHelper output;
+    private readonly ITestOutputHelper output; //in case we need to output to console
 
     public CreateBookingTests(ITestOutputHelper output)
     {
@@ -40,12 +40,6 @@ public class CreateBookingTests
             IsActive = true,
             RoomId = booking.RoomId,
         };
-        
-        /*
-        output.WriteLine(BookingTestData.StartDateData.ToString());
-        output.WriteLine(BookingTestData.EndDateData.ToString());
-        output.WriteLine(booking.ToString());
-        */
         
         //Act
         var result = await bookingManager.CreateBooking(newBooking); 
@@ -139,7 +133,115 @@ public class CreateBookingTests
         //Assert
         Assert.False(result);
         
-        
     }
 
+    //Case 16
+    [Theory]
+    [MemberData(
+        nameof(BookingTestData.OneRoomBookedData),
+        MemberType = typeof(BookingTestData))]
+    public async Task CreateBooking_BookingARoomThatDoesntExist_ShouldReturnTrueAnyways(Booking booking)
+    {
+        List<Room> rooms = [
+            new Room { Id = 1, Description = "Room 1" },
+            new Room { Id = 2, Description = "Room 2" },
+        ];
+        var bookingManager = MoqBookingManager.CreateBookingManager(rooms, new List<Booking>([booking]));
+
+        Booking newBooking = new Booking
+        {
+            Id = 44,
+            StartDate = BookingTestData.StartDateData,
+            EndDate = BookingTestData.EndDateData,
+            IsActive = true,
+            RoomId = 29803489,
+        };
+        
+        //Act
+        var result = await bookingManager.CreateBooking(newBooking); 
+
+        //Assert
+        Assert.True(result);
+    }
+    
+    //Case 17
+    [Theory]
+    [MemberData(
+        nameof(BookingTestData.OneRoomBookedData),
+        MemberType = typeof(BookingTestData))]
+    public async Task CreateBooking_StartDateIsExactlyOneMinuteAfterPreviousBookingEndDate_ShouldReturnTrue(Booking booking)
+    {
+        List<Room> rooms = [
+            new Room { Id = 1, Description = "Room 1" },
+        ];
+        var bookingManager = MoqBookingManager.CreateBookingManager(rooms, new List<Booking>([booking]));
+
+        Booking newBooking = new Booking
+        {
+            Id = 44,
+            StartDate = booking.EndDate.AddMinutes(1),
+            EndDate = booking.EndDate.AddDays(3),
+            IsActive = true,
+            RoomId = booking.RoomId,
+        };
+        
+        //Act
+        var result = await bookingManager.CreateBooking(newBooking); 
+
+        //Assert
+        Assert.True(result);
+    }
+    
+    //Case 18
+    [Theory]
+    [MemberData(
+        nameof(BookingTestData.OneRoomBookedData),
+        MemberType = typeof(BookingTestData))]
+    public async Task CreateBooking_EndDateIsExactlyOneMinuteBeforePreviousBookingStartDate_ShouldReturnTrue(Booking booking)
+    {
+        List<Room> rooms = [
+            new Room { Id = 1, Description = "Room 1" },
+        ];
+        var bookingManager = MoqBookingManager.CreateBookingManager(rooms, new List<Booking>([booking]));
+
+        Booking newBooking = new Booking
+        {
+            Id = 44,
+            StartDate = booking.StartDate.AddHours(-12),
+            EndDate = booking.StartDate.AddMinutes(-1),
+            IsActive = true,
+            RoomId = booking.RoomId,
+        };
+        
+        //Act
+        var result = await bookingManager.CreateBooking(newBooking); 
+
+        //Assert
+        Assert.True(result);
+    }
+    
+    //Case 19
+    [Fact]
+    public async Task CreateBooking_OneDayBooking_ShouldReturnTrue()
+    {
+        List<Room> rooms = [
+            new Room { Id = 1, Description = "Room 1" },
+        ];
+        var bookingManager = MoqBookingManager.CreateBookingManager(rooms, new List<Booking>());
+
+        Booking newBooking = new Booking
+        {
+            Id = 44,
+            StartDate = DateTime.Today.AddDays(2),
+            EndDate = DateTime.Today.AddDays(2),
+            IsActive = true,
+            RoomId = 1,
+        };
+        
+        //Act
+        var result = await bookingManager.CreateBooking(newBooking); 
+
+        //Assert
+        Assert.True(result);
+    }
 }
